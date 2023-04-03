7a67c088802d03cd1ccbf01f1ca9523cd240bd53
Vanessa <vndukuw@gmail.com>
2023-04-03 08:21:08 -0700

    The line of life is a ragged diagonal between duty and desire

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,19 +1,22 @@
 #include "main.h"
-
+#include <unistd.h>
 /**
- * print_chessboard - prints the chessboard
- * @a: two dimension array to print
+ * print_chessboard - Entry point
+ * @a: array
+ * _putchar - writes the character c to stdout
+ * Return: Always 0 (Success) 
  */
+void _putchar(void)
 void print_chessboard(char (*a)[8])
 {
-	int i, j;
+	int i;
+	int j;
 
 	for (i = 0; i < 8; i++)
 	{
 		for (j = 0; j < 8; j++)
-		{
 			_putchar(a[i][j]);
-		}
 		_putchar('\n');
 	}
 }
+
